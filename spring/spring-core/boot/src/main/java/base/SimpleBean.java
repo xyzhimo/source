ece19b4b4a796f12215b0d7c881c1ba1bde651c9
xiaoyizhi <18702528592@163.com>
2018-11-27 20:17:41 +0800

    :)feat) hmily2.0 boot init

--- spring/spring-core/boot/src/main/java/base/SimpleBean.java
+++ spring/spring-core/boot/src/main/java/base/SimpleBean.java
@@ -2,6 +2,12 @@ package base;
 
 public class SimpleBean {
 
+    private PropertyBean propertyBean;
+
+    public void setPropertyBean(PropertyBean propertyBean) {
+        this.propertyBean = propertyBean;
+    }
+
     public void send() {
         System.out.println("I am send method from SimpleBean!");
     }
